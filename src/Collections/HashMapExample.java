1b3545f287c0d7d076ec50820d392f7ca7394697
Vishnu Ramesh <r24vnath1994@gmail.com>
2025-02-16 14:37:56 +0530

    Java Finsih with collection

---
+++ src/Collections/HashMapExample.java
@@ -0,0 +1,23 @@
+package Collections;
+
+import java.util.HashMap;
+
+public class HashMapExample {
+    public static void main(String[] args) {
+
+        HashMap<Integer,String> hm = new HashMap<Integer,String>();
+        hm.put(3,"Vishnu");
+        hm.put(5,"Preethi");
+        hm.put(5,"Vanitha");
+        hm.put(6,"Ziggy");
+        hm.put(7,"Vichu");
+        hm.put(8,"Usha");
+        hm.put(9,"Ramesh");
+        hm.put(10,"Vishnu");
+        hm.put(11,"Love");
+        hm.put(12,"Preethi");
+        hm.put(13,"Extra");
+        System.out.println(hm);
+
+    }
+}
