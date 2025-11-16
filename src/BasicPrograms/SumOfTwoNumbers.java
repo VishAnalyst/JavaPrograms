e84791f2fe820bdd285854e8c45931aee6aff6e0
Vishnu Ramesh <r24vnath1994@gmail.com>
2025-11-16 17:34:55 +0530

    3programs added

---
+++ src/BasicPrograms/SumOfTwoNumbers.java
@@ -0,0 +1,16 @@
+package BasicPrograms;
+
+import AssignmentPrograms.sumOfDigits;
+
+public class SumOfTwoNumbers {
+    public static void main(String[] args) {
+    int a = 100;
+    int b = 200;
+    int sum = a+b;
+    System.out.println("Sum of two numbers is" + sum);
+    
+        
+    }
+    
+    
+}
