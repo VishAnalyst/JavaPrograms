f5dad5420995f6a4b60111248d092bb3661c55a9
Vishnu Ramesh <r24vnath1994@gmail.com>
2025-11-16 17:27:23 +0530

    1st program is written

---
+++ src/BasicPrograms/ProgramDay1.java
@@ -0,0 +1,8 @@
+package BasicPrograms;
+
+public class ProgramDay1 {
+    public static void main(String[] args) {
+        System.out.println("Program day 1 written");
+    }
+    
+}
---
+++ src/BasicPrograms/isTesterProgram.java
@@ -0,0 +1,14 @@
+package BasicPrograms;
+
+public class isTesterProgram {
+    public static void main(String[] args) {
+        String name = "Vishnu";
+        int age = 32;
+        boolean isTester = true;
+
+        System.out.println("name" + name);
+        System.out.println("Age: " + age);
+        System.out.println("Is he a tester" + isTester);
+    }
+    
+}
