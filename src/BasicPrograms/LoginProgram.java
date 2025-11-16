0f09346dcd3a12b7c48828c0ec95176f55ed2b99
Vishnu Ramesh <r24vnath1994@gmail.com>
2025-11-16 23:17:10 +0530

    login program added

---
+++ src/BasicPrograms/LoginProgram.java
@@ -0,0 +1,15 @@
+package BasicPrograms;
+
+public class LoginProgram {
+    public static void main(String[] args) {
+        String email = "test@gmail.com";
+        String password = "abc@1234";
+        
+        if (email=="test@gmail.com" && password=="abc@123") {
+            System.out.println("LOGIN SUCCESSFUL");
+        }else {
+            System.out.println("Login FAILED");
+        }
+    }
+    
+}
---
+++ src/BasicPrograms/positiveNegativeNumber.java
@@ -0,0 +1,21 @@
+package BasicPrograms;
+
+import AssignmentPrograms.sumOfDigits;
+
+public class positiveNegativeNumber {
+    public static void main(String[] args) {
+        int num = -1;
+        String positive = "POSITIVE NUMBER";
+        String negative = "NEGATIVE NUMBER";
+        String zero = "Number is ZERO";
+
+        if (num>=0) {
+            System.out.println(positive);
+        }else if(num<=0){
+            System.out.println(negative);
+        }else if (num==0) {
+            System.out.println(zero);
+        }
+    }
+    
+}
