6b696c9ec56dfe01d825f1d43fea35ded7420e8f
Vishnu Ramesh <r24vnath1994@gmail.com>
2025-12-29 13:04:29 +0530

    Latest push

--- src/LoopPrograms/printReverseOfANumber.java
+++ src/LoopPrograms/printReverseOfANumber.java
@@ -4,12 +4,11 @@ import AssignmentPrograms.sumOfDigits;
 
 public class printReverseOfANumber {
     public static void main(String[] args) {
-        int num = 123456;
+        int num = 12345;
         int rev = 0;
-        for(; num!=0; num=num/10){
-            int digit = num%10;
-            rev = rev * 10 + digit;
-            
+        for(;num!=0; num=num/10){
+        int digit = num%10;
+        rev = rev*10+digit;
         }System.out.println(rev);
     }
 }
